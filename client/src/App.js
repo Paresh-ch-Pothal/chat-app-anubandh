bc050a2091a3178b88aa78ef9797ec4dd9463bcf
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-10-06 20:35:55 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <div style={{ backgroundColor: "#2a2d33" }}>
+        <Home/>
       </div>
     </>
   );
---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='home'>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Home

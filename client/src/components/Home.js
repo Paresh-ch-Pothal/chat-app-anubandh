d90fe38d4f4cd6f6b1ce0126b9d0c859e433e540
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-10-06 21:36:24 +0530

    updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { IoSend } from "react-icons/io5";
 
 const Home = () => {
-  return (
-    <div>
-      <div className='home'>
-        <div className='left'></div>
-        <div className='right'></div>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className='home'>
+                <div className='left'></div>
+                <div className='right'>
+                    <div className='righttop'>
+                        hello
+                    </div>
+                    <div className='rightcenter'></div>
+                    <div className='rightbottom'>
+                        <div className="sendbar">
+                            <input type="text" placeholder='Message' class="form-control" aria-label="Dollar amount (with dot and two decimal places)"/>
+                            <IoSend className='sendbutton' color='#e50a82' size={25} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home

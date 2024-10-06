3eee26955571abeaf4e9445f3a2e6808469610f0
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-10-06 20:06:36 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home';
 
 
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <>
       <div style={{ backgroundColor: "#2a2d33" }}>
-        <Home/>
       </div>
     </>
   );

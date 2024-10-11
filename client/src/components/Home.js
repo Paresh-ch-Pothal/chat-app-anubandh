06f033d07b036c5ce497796bdb2637058334f549
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-10-11 15:49:48 +0530

    updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { IoSend } from "react-icons/io5";
+import { FaUserGraduate } from "react-icons/fa6";
+import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
+import { MdKeyboardBackspace } from "react-icons/md";
+
 
 const Home = () => {
     return (
@@ -8,13 +12,17 @@ const Home = () => {
                 <div className='left'></div>
                 <div className='right'>
                     <div className='righttop'>
-                        hello
+                        <div className='user'><FaUserGraduate size={25} style={{cursor: "pointer"}}/>
+                        <MdKeyboardBackspace className='backicon' size={25} style={{cursor: "pointer"}}/></div>
+                        
+                        <div>Name Of The User</div>
+                        <PiDotsThreeOutlineVerticalFill size={25} style={{cursor: "pointer"}}/>
                     </div>
                     <div className='rightcenter'></div>
                     <div className='rightbottom'>
                         <div className="sendbar">
                             <input type="text" placeholder='Message' class="form-control" aria-label="Dollar amount (with dot and two decimal places)"/>
-                            <IoSend className='sendbutton' color='#e50a82' size={25} />
+                            <IoSend style={{cursor: "pointer"}} className='sendbutton'  color='#e50a82' size={25} />
                         </div>
                     </div>
                 </div>

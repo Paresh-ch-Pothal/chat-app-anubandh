ce3a60f02009581a4af9ac5e117ffe6ad80562cf
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2024-10-13 21:33:02 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -17,6 +17,41 @@ app.use("/user",userRoutes);
 app.use("/chat",chatRoutes);
 app.use("/message",messageRoutes);
 
-app.listen(PORT,()=>{
+const server=app.listen(PORT,()=>{
     console.log("Server is running on port 5000");
+})
+
+const io=require("socket.io")(server,{
+    pingTimeout: 60000,
+    cors:{
+        origin: "http://localhost:3000"
+    }
+})
+io.on("connection",(socket)=>{
+    console.log("Connected to socket.io")
+    socket.on('setup',(userData)=>{
+        socket.join(userData._id)
+        socket.emit("Connected")
+    })
+    socket.on('join chat',(room)=>{
+        socket.join(room);
+        console.log("User Joined room: "+room);
+    })
+
+    socket.on("new message",(newMessageReceived)=>{
+        var chat=newMessageReceived.chat;
+        if(!chat.participants){
+            return console.log("Chat participants are not defined");
+        }
+        chat.participants.forEach(user => {
+            if(user._id == newMessageReceived.sender._id) return;
+            socket.in(user._id).emit("message received",newMessageReceived)
+        })
+
+    })
+
+    socket.off("setup",()=>{
+        console.log("user is Disconnected");
+        socket.leave(userData._id);
+    })
 })
